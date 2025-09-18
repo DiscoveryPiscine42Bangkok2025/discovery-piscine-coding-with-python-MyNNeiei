60259b6cf0eaa1bb22258a63e3d24a757b735ec9
MyNNeiei <kullananzaza@gmail.com>
2025-09-18 22:35:02 +0700

    miniproj2

--- miniproject/Rush00/ex00/main.py
+++ miniproject/Rush00/ex00/main.py
@@ -1,8 +1,8 @@
 from checkmate import checkmate
 
 def main():
-    checkmate(123)
-    checkmate("")
+    checkmate(123) # Error
+    checkmate("") # Error
 
     board1 = """\
 R...
@@ -10,85 +10,86 @@ R...
 ..P.
 ....\
 """
-    checkmate(board1)
+    checkmate(board1) # Success
 
     board2 = """\
 ..
 .K"""
-    checkmate(board2)
+    checkmate(board2) # Fail
 
     board3 = """\
 K...
 ..
 ...."""
-    checkmate(board3)
+    checkmate(board3) # Error
 
     board4 = """\
 K..
 ..."""
-    checkmate(board4)
+    checkmate(board4)# Error
 
     board5 = """\
 ....
 ....
 ....
 ...."""
-    checkmate(board5)
+    checkmate(board5) # Error
 
     board6 = """\
 K...
 ....
 ....
 ...K"""
-    checkmate(board6)
+    checkmate(board6) # Error
 
     board7 = """\
 ....
 RK..
 ....
 ...."""
-    checkmate(board7)
+    checkmate(board7) # Success
 
     board8 = """\
 .R..
 ....
 ....
 .K.."""
-    checkmate(board8)
+    checkmate(board8) # Success
 
     board9 = """\
 B...
 ....
 ....
 ...K"""
-    checkmate(board9)
+    checkmate(board9) # Success
 
     board10 = """\
 ....
 QK..
 ....
 ...."""
-    checkmate(board10)
+    checkmate(board10) # Success
 
     board11 = """\
 Q...
 ....
 ....
 ...K"""
-    checkmate(board11)
+    checkmate(board11) # Success
 
     board12 = """\
 ....
 ....
 ....
 ...K"""
+    checkmate(board12) # Fail
 
     board13 = """\
 R.P.
 ....
 ....
 ...K"""
-    checkmate(board13)
+    checkmate(board13) # Fail
 
 if __name__ == "__main__":
     main()
